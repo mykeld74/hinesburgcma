743c735d62a93c0aab1da4fd7b5339a10ff7f6e5
mykeld74 <mike@msdwebsolutions.com>
2026-01-17 09:35:57 -0700

    move Our Hope

--- src/routes/about-us/our-story/+page.svelte
+++ src/routes/about-us/our-story/+page.svelte
@@ -17,6 +17,28 @@
 
 <section class="pageSection blockSection">
 	<div class="blockSectionContent">
+		<h2 class="pageSectionTitle">Our Hope</h2>
+		<p class="pageSectionText">
+			God hates loneliness! That’s why Jesus is at work in the world rescuing and restoring all of
+			creation – including us! Life is filled with overwhelming circumstances and lots of questions
+			like, <em
+				>"How do I deal with my fears, my insecurities, my anxiety and the darkness that steals my
+				joy?"</em
+			>
+			Whether it's financial issues, relationship issues, or spiritual issues - Jesus has the answer:
+			It’s all about <strong>REAL</strong> life with <strong>REAL</strong> people in
+			<strong>TRUE</strong>
+			community. That’s our <strong>HOPE</strong> for you!
+			<em
+				>"Christ in you, the <strong>HOPE</strong>
+				of glory."</em
+			> Colossians 1:27
+		</p>
+	</div>
+</section>
+
+<section class="pageSection blockSection">
+	<div class="blockSectionContent">
 		<h2 class="pageSectionTitle">Our Why</h2>
 		<p class="pageSectionText">
 			Most people live disconnected from God, His family, and their purpose. We exist to invite
@@ -40,27 +62,6 @@
 		<p class="pageSectionText">
 			To see every corner of our world filled with the love and good news of Jesus by making
 			disciples who know how to make disciples at church, home, school, work, and the neighborhood.
-		</p>
-	</div>
-</section>
-<section class="pageSection blockSection">
-	<div class="blockSectionContent">
-		<h2 class="pageSectionTitle">Our Hope</h2>
-		<p class="pageSectionText">
-			God hates loneliness! That’s why Jesus is at work in the world rescuing and restoring all of
-			creation – including us! Life is filled with overwhelming circumstances and lots of questions
-			like, <em
-				>"How do I deal with my fears, my insecurities, my anxiety and the darkness that steals my
-				joy?"</em
-			>
-			Whether it's financial issues, relationship issues, or spiritual issues - Jesus has the answer:
-			It’s all about <strong>REAL</strong> life with <strong>REAL</strong> people in
-			<strong>TRUE</strong>
-			community. That’s our <strong>HOPE</strong> for you!
-			<em
-				>"Christ in you, the <strong>HOPE</strong>
-				of glory."</em
-			> Colossians 1:27
 		</p>
 	</div>
 </section>
