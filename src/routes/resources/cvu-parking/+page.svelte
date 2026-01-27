874ac455b5899f47bb7a295b6e0d474ac1dd1782
mykeld74 <mike@msdwebsolutions.com>
2026-01-27 12:24:59 -0700

    update cvu parking page

--- src/routes/resources/cvu-parking/+page.svelte
+++ src/routes/resources/cvu-parking/+page.svelte
@@ -20,26 +20,23 @@
 	<div class="pageHeroContent">
 		<h1 class="pageHeroChild pageHeroTitle">CVU Parking</h1>
 		<p class="pageHeroChild pageHeroText">
-			CAC is grateful to support Champlain Valley Union High School by offering parking spaces in
-			our front parking lot, for juniors (11th grade) attending CVU. When we can, we also love to
-			come out with our coffee cart to bless our students with coffee during cold months, and with
-			cold refreshments near the end of the year!
+			Community Alliance Church (CAC) is grateful to support Champlain Valley Union High School by
+			setting aside a limited number of parking spaces on our campus for CVU students. You may even
+			see us handing out hot cocoa and treats to the students on Friday mornings!
 		</p>
 	</div>
 </section>
 
 <section class="pageSection alertSection">
 	<div class="pageSectionText">
-		<p><strong>{statusMessage.current}</strong></p>
 		<p>
-			<strong
-				>Parking will open for the 2025-2026 school year on {statusMessage.nextYear.date}:</strong
-			>
-			<a
-				href="https://communityalliancechurch.churchcenter.com/registrations/events/2897978"
-				target="_blank"
-				rel="noopener noreferrer">{statusMessage.nextYear.link}</a
-			>
+			<strong>At this time, parking passes are SOLD OUT for the 2025-2026 school year. </strong>
+		</p>
+		<p>
+			Registration for the 2026-2027 school year will open after results from the CVU Junior Parking
+			Lottery have been announced in mid-May 2026. Students who will be juniors in 2026-2027 and
+			were unable to secure a spot in J Lot will be given preference at the church lot. Check back
+			here in May for a live registration link!
 		</p>
 	</div>
 </section>
@@ -48,12 +45,39 @@
 	<h2 class="pageSectionTitle">Parking Pass Information</h2>
 	<div class="pageSectionText">
 		<p>
-			Passes are available for 35 parking spots and typically are reserved during the spring
-			<em>preceding</em> the school year for which the pass will be valid. Passes are issued on a first-come,
-			first-served basis, and are good for the duration of the school year. Physical passes are distributed
-			to students at the start of the fall school semester, so there's no chance of losing them over
-			the summer. The fee for reserving a spot is $50. This fee is used to support the work of the Hinesburg
-			Community Resource Center's Food Shelf.
+			Parking passes are available for up to 30 spaces each school year in the CAC parking lot. The
+			fee for reserving a spot is $50. This fee is used to support the work of the Hinesburg
+			Community Resource Center's Food Shelf. Parking passes typically are reserved in the spring
+			preceding the school year for which the pass will be valid. Preference is given to students
+			who will be juniors at CVU during the year the pass will be valid. Arrangements are made with
+			student passholders to pick up the physical pass from the church, usually the week before
+			school begins in the fall.
+		</p>
+		<p>
+			A waiting list is maintained for the current school year, should a pass/space become
+			available. You may email us any time during the current school year to have your name placed
+			on the list. Please be advised that the waiting list is typically 15-25 students long and CVU
+			juniors will be given priority. We do not reserve parking spaces for years beyond the current
+			school year.
+		</p>
+		<p>
+			CAC parking passes are NON-TRANSFERRABLE. CAC passholders may not give or loan passes out to
+			other students or family members at any time, regardless of current need or prior payment
+			made. Any transfer of a CAC parking pass without direct consent of CAC will render the pass
+			inactive for that school year and the student(s) with the pass will no longer be authorized to
+			park at CAC.
+		</p>
+		<p>
+			If you no longer need your CAC parking pass, please contact Amanda Wheeler for instructions on
+			how to return it. Your returned pass will be assigned to the next student on our waiting list
+			and we will issue you a prorated refund on your payment.
+		</p>
+		<p>
+			Students are only eligible for a parking pass at CAC if they do not hold an active CVU parking
+			pass. Please be aware that we do monitor our parking lot for compliance weekly. We also
+			collaborate with CVU on parking matters, which includes sharing of passholder information
+			(name, car, plate number, pass numbers). This increases our ability to maintain a safe,
+			equitable parking situation for students on both campuses.
 		</p>
 	</div>
 </section>
@@ -73,15 +97,17 @@
 <section class="pageSection">
 	<h2 class="pageSectionTitle">Contact</h2>
 	<p class="pageSectionText">
-		If you are interested in learning more about parking at CAC, please contact Connor Durochia.
+		If you are interested in learning more about parking at CAC, please email <a
+			href="mailto:awheeler@hinesburgcma.org">Amanda Wheeler</a
+		>.
 	</p>
-	<div class="pageHeroActions">
+	<!-- <div class="pageHeroActions">
 		<a
 			href="https://communityalliancechurch.churchcenter.com/registrations/events/2897978"
 			target="_blank"
 			rel="noopener noreferrer">Register Here</a
 		>
-	</div>
+	</div> -->
 </section>
 
 <style>
